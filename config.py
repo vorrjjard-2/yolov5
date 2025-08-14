5bf9c6502f0c582db1100fee3e09583c355782ef
vorrjjard <ruben.saulog@gmail.com>
2025-08-14 14:28:16 +0800

    wrote transformations

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 import utils
 
+import torch
+
 import cv2
 
 import albumentations as A
@@ -16,10 +18,27 @@ N_CLASSES = 20
 train_transform = A.Compose([
     A.Mosaic(grid_yx=(2,2), target_size=[IMG_SIZE, IMG_SIZE]),
     A.HorizontalFlip(p=0.5),
-    A.VerticalFlip(p=0.3),
-    A.Affine(scale=(0.8, 1.2), translate_percent=(0.2, 0.2), rotate=(-30, 30), shear=(0.1, 0.1), border_mode=cv2.BORDER_CONSTANT) # FIX THIS BULLSHIT
-    
-    ],bbox_params=A.BboxParams(format='coco', label_fields=[])
+    A.Perspective(scale=(0.0, 0.0005), keep_size=True, pad_mode=cv2.BORDER_CONSTANT, pad_val=(114,114,114), p=0.2),
+    A.Affine(
+        scale=(0.8, 1.2),
+        translate_percent=(0.2, 0.2), 
+        rotate=(-30, 30), 
+        shear=(0.1, 0.1), 
+        border_mode=cv2.BORDER_CONSTANT), # FIX THIS BULLSHIT
+
+    A.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=70, val_shift_limit=40, p=0.9),
+
+    A.LongestMaxSize(max_size=IMG_SIZE),
+    A.PadIfNeeded(min_height=IMG_SIZE, min_width=IMG_SIZE, border_mode=cv2.BORDER_CONSTANT, fill=(114,114,114)),
+
+    A.ToTensorV2()
+    ],bbox_params=A.BboxParams(
+        format='coco', 
+        label_fields=['labels'],
+        min_area=4
+        ,min_visibility=0.1,
+        check_each_transform=True
+        )
     )
 
 
